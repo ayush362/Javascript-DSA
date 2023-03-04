6fba81f9a94dc5e220fa2a4715e2fe1f5c27262b
Ayush Khatri <ayushkhatri362@gmail.com>
2023-03-04 17:36:12 +0530

    odd2: Formatting changes

--- recursion/odd2.js
+++ recursion/odd2.js
@@ -1,16 +1,13 @@
-function checkodd(arr)
-{
-    newarr = []
-    if(arr.length===0)
-    {
-        return newarr;
+function checkOdd(arr) {
+    newArr = []
+    if (arr.length === 0) {
+        return newArr;
     }
-    if(arr[0]%2 !==0)
-    {
-        newarr.push(arr[0])
+    if (arr[0] % 2 !== 0) {
+        newArr.push(arr[0])
     }
-    newarr = newarr.concat(checkodd(arr.slice(1)))
-    return newarr;
+    newArr = newArr.concat(checkOdd(arr.slice(1)))
+    return newArr;
 }
 
-console.log(checkodd([1,2,3,4,5,6,7,8,9]))
+console.log(checkOdd([1, 2, 3, 4, 5, 6, 7, 8, 9]))
