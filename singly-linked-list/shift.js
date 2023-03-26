8a43e1ebe789d1c969ac4e13e3326e6767086f7b
Ayush Khatri <ayushkhatri362@gmail.com>
2023-03-26 22:19:59 +0530

    Added shift program which removes the element from the begining of the list

---
+++ singly-linked-list/shift.js
@@ -0,0 +1,77 @@
+class node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class singlylinkedlist {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+    push(val) {
+        let newNode = new node(val)
+        if (!(this.head)) {
+            this.head = newNode
+            this.tail = this.head
+        }
+        else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+    pop() {
+        let current = this.head
+        let newTail = current
+        if(!(this.head)) return undefined
+        else
+        {
+        while (current.next) {
+            newTail = current
+            current = current.next
+        }
+        this.tail = newTail
+        this.tail.next = null
+        this.length--
+        if(this.length === 0)
+        {
+            this.head = null
+            this.head = null
+        }
+        console.log(`The poped out item is ${current.val}`)
+    }
+    }
+    shift()
+    {
+        if(!(this.head)) return undefined
+        else{
+            let temp = this.head
+            this.head = temp.next
+            this.length-- 
+            console.log(`The item removed from the beggining is ${temp.val}`)
+
+        }
+    }
+    printList() {
+       let current = this.head
+            while(current)
+            {
+                console.log(current.val)
+                current=current.next
+            }
+        }
+    }
+let list = new singlylinkedlist()
+list.push("My")
+list.push("Name")
+list.push("is")
+list.push("Ayush")
+list.push("Khatri")
+
+list.pop()
+list.shift()
+list.printList()
