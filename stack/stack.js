c593c78d15bd006fe71b08520d05c85405d3d20e
Ayush Khatri <ayushkhatri362@gmail.com>
2023-04-16 12:32:40 +0530

    Added a new program of stack

---
+++ stack/stack.js
@@ -0,0 +1,64 @@
+class node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    push(val) {
+        let NewNode = new node(val)
+        if (!this.first) {
+            this.first = NewNode;
+            this.last = NewNode;
+        }
+        else {
+            let temp = this.first;
+            this.first = NewNode;
+            this.first.next = temp;
+        }
+        this.size++;
+    }
+    pop() {
+        let temp = this.first
+        if (!this.first) {
+            return null;
+        }
+        else {
+            if (this.first === this.last) {
+                this.last = null
+            }
+            this.first = this.first.next;
+            this.size--;
+        }
+        return temp.val;
+    }
+    print() {
+        let current = this.first
+        if (!this.first) {
+            return null
+        }
+        else {
+            while (current) {
+                console.log(current.val)
+                current = current.next
+            }
+        }
+    }
+}
+
+let list = new stack();
+list.push("Hello");
+list.push("My");
+list.push("Name")
+// list.print()
+list.pop();
+list.pop();
+// list.pop();
+list.print()
+// console.log(list)
